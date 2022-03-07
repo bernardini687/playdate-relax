bed73b043752fc1573e5824f90cf972f802e5d57
oscar bernardini <bernardini687@gmail.com>
2022-03-07 10:48:51 +0100

    optimize: avoid updating how-to text and importing CoreLibs/graphics

--- Source/main.lua
+++ Source/main.lua
@@ -1,12 +1,10 @@
-import "CoreLibs/graphics"
+-- import "CoreLibs/graphics"
 
 local gfx = playdate.graphics
 
---[[
-    Lua arrays are 1-indexed but here I find it
-    easier to use a 0-based cursor to cycle through
-    the array elements with the modulo operator.
-]]
+-- Lua arrays are 1-indexed but here I find it
+-- easier to use a 0-based cursor to cycle through
+-- the array elements with the modulo operator.
 local exerciseCursor = 0
 local exercises = {
     {
@@ -35,22 +33,25 @@ local exerciseProps = {
 
 local howToText = "➡️ / Ⓐ Next\n⬅️ / Ⓑ Info"
 local howToTextWidth, howToTextHeight = gfx.getTextSize(howToText)
-local _, exerciseTextHeigth = gfx.getTextSize("Breathing") -- Should cover ascendants and descendants.
+local _, exerciseTextHeigth = gfx.getTextSize("Breathing") -- Covers both ascendants and descendants.
 
 playdate.display.setInverted(true)
-gfx.setColor(gfx.kColorWhite) -- But it's black.
+gfx.setColor(gfx.kColorWhite) -- Fill with black.
+
+-- How-to text is fixed.
+gfx.drawText(howToText, 400 - howToTextWidth, 240 - howToTextHeight)
 
 function playdate.update()
     -- Let's offset the cursors by 1 since Lua arrays are 1-indexed.
     local currentExercise = exercises[exerciseCursor + 1]
     local currentExerciseProp = exerciseProps[exercisePropsCursor + 1]
 
+    -- Draw enough black background across the exercise information area,
+    -- then draw the current selected information on top of it.
     gfx.fillRect(0, 240 - exerciseTextHeigth, 400 - howToTextWidth, howToTextHeight)
     gfx.drawText(currentExercise[currentExerciseProp], 0, 240 - exerciseTextHeigth)
-    gfx.drawText(howToText, 400 - howToTextWidth, 240 - howToTextHeight)
 
-    -- The cycling-cursor logic could be abstracted.
-    -- Does it only need an array and the cursor state?
+    -- TODO: The cycling-cursor logic could be abstracted. Does it only need an array and the cursor state?
     if playdate.buttonJustPressed("a") then
         exerciseCursor += 1
         exerciseCursor %= #exercises
